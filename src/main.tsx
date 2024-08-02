8589a32fb1904fe6a1020b61f89e48281404bfcc
NewTanachot <TanachotNew2543@gmail.com>
2024-08-02 23:58:14 +0700

    add basename

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="homepage-vite-test">
             <App />
         </BrowserRouter>
     </React.StrictMode>
